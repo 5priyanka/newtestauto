4893c6681d09712425886f160da5dead17d0c908
priyanka <priyanka.j@hulkapps.com>
2021-08-23 11:24:26 +0530

    latest

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/users/test', (req, res) => {
-res.send({ msg: 'testing'});
+res.send({ msg: 'hello world'});
 })
 
 app.listen(5000, () => {
